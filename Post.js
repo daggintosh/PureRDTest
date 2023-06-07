245cb7b2b46dad7d5f9a5fac22b8a7a19211fad3
Dagg <dagg@kyzune.com>
2023-06-06 18:42:24 -0700

    Don't insert unnecessary fields

--- Post.js
+++ Post.js
@@ -5,19 +5,18 @@ fetch(`https://www.reddit.com/${id}.json`)
     .then(result => result.json())
     .then(result => {
         const div = document.getElementById("Post")
-        const { title, created_utc, subreddit_name_prefixed, ups, selftext, url, media  } = result[0].data.children[0].data
+        const { title, created_utc, author, post_hint, subreddit_name_prefixed, ups, selftext, url, media  } = result[0].data.children[0].data
         let video = media?.reddit_video.hls_url
         document.title = title
         div.innerHTML = `
         <h2>${title}</h2>
         <h4>${subreddit_name_prefixed}</h4>
+        <h4>${author}</h4>
         <p>${ups} Upvotes</p>
-        <p>${selftext}</p>
-        <a href="${url}">${url}</a>`
-        + (media
-            ? `<video id="video"><source src=\"${media?.reddit_video.hls_url}\"></video>`
-            : `<img src="${url}" alt="Reddit doesn't have alt text"></img>`
-        )
+        <p>${selftext}</p>`
+        + (post_hint === "link" ? `<a href="${url}">${url}</a>` : "")
+        + (media ? `<video id="video"><source src=\"${media?.reddit_video.hls_url}\"></video>` : "")
+        + (post_hint === "image" ? `<img src="${url}" alt="Reddit doesn't have alt text"></img>` : "")
 
         const videoPlayer = document.getElementById("video")
         if (videoPlayer) videoPlayer.setAttribute("controls", "controls")
